a54845ff91041440ee59fdcfb8c042c852f58e3c
Maheshderengula <derengulamahesh7@gmail.com>
2025-12-17 10:09:16 +0530

    task

---
+++ dsiterate.py
@@ -0,0 +1,15 @@
+text={
+    "name":"mahii",
+    "age":21,
+    "collage":"emeralds",
+    "city":"hyd"
+
+}
+
+# duplicate keys are not allowed in dict 
+# dict is amutable object
+
+print(text)
+text["city"]="bgr"
+print(text)
+
