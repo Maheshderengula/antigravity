f51ca23034bba7e3a2a4a6b9a372e42128276ad0
Maheshderengula <derengulamahesh7@gmail.com>
2025-12-17 15:29:27 +0530

    main

---
+++ number.py
@@ -0,0 +1,20 @@
+num = 5 + 2.0
+print(num)
+print(type(num))
+
+# typecasting to int
+ num_int = int (5.7)
+ print(num)
+
+# typecasting to float
+num_float = float("4.13")
+print(num_float)
+
+# typecasting to string 
+message = str (40)
+print(message)
+
+# typecasting to boolean 
+is_ture = bool(2)
+is_false = bool(0)
+print(is_false)
---
+++ program.py
@@ -0,0 +1,5 @@
+a = 100
+b = 1.3
+c = a+b 
+print(type(b))
+print(type(c))
