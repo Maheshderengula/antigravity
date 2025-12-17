67dd696e0f9ecd9455d717c97fb633fd184bf4e9
Maheshderengula <derengulamahesh7@gmail.com>
2025-12-17 15:45:13 +0530

    main

---
+++ orded.py
@@ -0,0 +1,11 @@
+char_a ='A'
+char_b ='B'
+ascii_value_a = ord(char_a)
+ascii_value_b = ord(char_b)
+print ("ASCII value of 'A'is:",ascii_value_a)
+print ("ASCII value of 'B'is:",ascii_value_b)
+
+
+ascii_value = 62
+character = chr(ascii_value)
+print("charater of ASCII value 65 is:", character)
