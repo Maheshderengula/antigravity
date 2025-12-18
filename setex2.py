502a4fdee0d24c8635932970e1f0f9b83366b672
Maheshderengula <derengulamahesh7@gmail.com>
2025-12-18 10:46:44 +0530

    "gm"

--- setex2.py
+++ setex2.py
@@ -1,3 +1,20 @@
-st-names={'raju','mahi','abhi','jeevi',}
-for name in st-names:
-    print(name)
+st1-names={'raju','mahi','abhi','jeevi',}
+st2-names={'ramesh','heam','lax','ramana'}
+
+# set union using .union()
+print(st1.union(st2))
+
+#set union using the | operator
+print(st1 | st2)
+
+A = {1, 2, 4, 6, 8}
+B = {1, 2, 3, 4, 5}
+ 
+# Use the method
+print(A.intersection(B))
+ 
+# Use the & operator
+print(A & B)
+ 
+# Output:
+# {1, 2, 4}
