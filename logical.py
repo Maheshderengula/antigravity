6a9d7cdf054b370951ba63c20a2bc01de9354205
Maheshderengula <derengulamahesh7@gmail.com>
2025-12-22 11:41:28 +0530

    example

--- logical.py
+++ logical.py
@@ -1,19 +1,17 @@
-eid=[101,102,103,104]
-print(101 in eids) #ture
-print(108 in eids) #false
-print(108 not eids) #ture
-enames=("raji","rahul","mahi","jevvi")
-print('raji'in enames)#ture
-print('mahhi'in enames)#false
-sizes={'s','m','xl'}
-print('xxl'not in sizes)#ture
-ename="raji"
-print('a' in ename)#ture
-b=bytes([10,20,30,40])
+#logical operator: AND
+x = 1
+y = 1
 
-ba=bytearray([10,20,30,40])
-print(10 in ba)
-numbers=range(100)
-print(100 in eids) #ture
-print(99 in eids) #false
-print(98 in eids) #ture
+print(x and y)
+
+# logical operator: OR
+q = 1
+l = 0
+print(q or l)
+
+# logical operator :NOT
+P = 0
+ 
+print( not P) 
+
+
---
+++ membership.py
@@ -0,0 +1,17 @@
+# list of names
+t =['raji','ramesh','abi','sa',]
+print('raji' in t )
+print('jevvi'not in t )
+
+
+# list of fruits
+fruits = ['apple','banana','mengo','orange','grpe']
+# check if 'apple' is in the list 
+print('apple'in fruits)
+
+#check if 'watermelon'is in the list 
+print('watermelon' in  fruits)
+
+
+
+
