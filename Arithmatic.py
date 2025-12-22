ad50ff6b39b51175d1b118a6dfcc1b4580ac857a
Maheshderengula <derengulamahesh7@gmail.com>
2025-12-22 10:57:33 +0530

    "example"

--- Arithmatic.py
+++ Arithmatic.py
@@ -1,13 +1,28 @@
+#addition
 a=10
 b=33.1
-c=40.5
-d=20
-e="raju"
-f="mahi"
-g= ture 
-h=false #false is considered as zero
-print(a+b)
-print(b+c)
-print(c+d)
-print(d+e)
-print(e+f)
+mahi = (a+b)
+
+print(mahi)
+
+#subtraction
+c = 30
+d = 60
+raju = (d-c)
+print(raju)
+
+
+
+
+
+ # integer 
+ i = 10
+ j = 3
+ jevvi = i // j 
+ print(jeevi)
+  
+  #modulo (remainder)
+  k = 10
+  l = 3
+  mahi = k % l 
+  print(mahi)
---
+++ comparision.py
@@ -0,0 +1,44 @@
+a = 10
+b = 30
+
+
+print (a==b)
+
+# comparision operator :not equal to (!=)
+c = 40
+d = 50
+
+
+print(c!=d)
+
+#comparisioon operator : greater than (>)
+
+h = 60
+r = 70 
+ 
+
+print(h>r)
+
+# comparision operator : less than (<)
+s = 80
+t = 90
+
+print(s<t)
+
+# comparision opertor : greater than or equal to (>=)
+l = 3
+m = 3
+print(l>=m)
+
+# comparision opertor : less than or equal to (<=)
+x = 10 
+y = 15
+
+print(x<=y)
+  
+
+
+
+ 
+   
+   
